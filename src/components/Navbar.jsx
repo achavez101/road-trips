4e4bce2e32c8ff2e7829b4953eacacfe62e90857
achavez101 <amairanygchavez@gmail.com>
2024-06-07 22:27:42 -0700

    navbar working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
   return (
     <div className='absolute w-full flex justify-between p-4 items-center'>
         <h1 className='text-white font-bold text-2xl z-20'>Experiences</h1>
-        <TfiMenu className='z-20 text-white cursor-pointer' size={25} />
+        <TfiMenu onClick={handleNav} className='z-20 text-white cursor-pointer' size={25} />
         <div 
             className={
                 nav 
-                    ? 'fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10' 
+                    ? 'ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10' 
                     : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10'
                 } 
         >
@@ -32,4 +32,4 @@ const Navbar = () => {
 
 export default Navbar
 
-// 30:18 minutes on video
+// 31:18 minutes on video
