42b25f7f055d7ea92f57bdc4cd7cef430701d2b5
achavez101 <amairanygchavez@gmail.com>
2024-06-10 21:24:39 -0700

    experiences refinement

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -6,7 +6,13 @@ import elkGrove from '../images/elk-grove.jpeg';
 
 const Experiences = () => {
   return (
-    <div className='max-w-[1400px] h-full bg-blue-200 mx-auto my-20 pt-16 lg:mb-[20%] md:mb[35%] px-4 grid lg:grid-cols-3 gap-4 pb-2'>
+    // look through experiences
+    // 'max-w-[1400px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4' 
+
+// old
+//'max-w-[1400px] h-full bg-blue-200 mx-auto my-20 pt-16 lg:mb-[20%] md:mb[35%] px-4 grid lg:grid-cols-3 gap-4 pb-2'
+
+    <div className='max-w-[1400px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4'>
         <div className='lg:top-20 relative lg:col-span-1 col-span-2'>
             <h3 className='text-2xl font-bold'>Experiences</h3>
             <p className='pt-4'>Enjoying the ride</p>
