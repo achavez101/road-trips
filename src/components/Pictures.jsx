5330f8b7c8a8180767fce7c84acaae7ad4ea75c0
achavez101 <amairanygchavez@gmail.com>
2024-06-12 14:38:20 -0700

    pictures edits

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,330 +1,21 @@
 import React, {useState} from 'react';
-
 import Hero from '../images/hero.JPG'
-
 import aguilarFamily from '../images/aguilarFamily.jpg'
-
 import centennialPark from '../images/centennialPark.JPG'
-// done
 import imperial from '../images/imperialBeach.JPG'
-
 import laJolla from '../images/laJolla.JPG'
-
 import laJolla2 from '../images/laJolla2.JPG'
-
 import cove from '../images/laJollaCove.JPG'
-
-//done
 import littleItaly from '../images/littleItaly.JPG'
-//done
 import littleItaly2 from '../images/littleItaly2.JPG'
-
 import oldTown from '../images/oldTown.JPG'
-
 import townBeca from '../images/oldTownBeca.jpg'
-
 import townRose from '../images/oldTownRose.jpg'
-
 import sdPier from '../images/sdPier.JPG'
-//done
 import seaport2 from '../images/seaport2.JPG'
-
 import seaportFriends from '../images/seaportFriends.jpg'
-
 import seaportMore from '../images/seaportMore.JPG'
-
-
-// const Pictures = () => {
-//   return (
-//     <div>
-//         {/* San Diego Pictures Section */}
-//         <h3 className='text-2xl font-bold p-2'>San Diego</h3>
-        
-//         {/* div for picture start so that they wrap */}
-//         <div className='flex flex-wrap p-2'>
-
-//         {/* seaport image */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image Seaport */}
-//         <img src={coronado} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 SeaPort Village
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>Beautiful place to enjoy the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={laJolla} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 La Jolla
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={cove} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 La Jolla Cove
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={littleItaly} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Little Italy
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={seaport2} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 SeaPort Village
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={sdPier} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 San Diego Pier
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={oldTown} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Old Town San Diego
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={Hero} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 SeaPort Village
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-
-//          {/* next start */}
-//          <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={townBeca} alt='' loading='eager' className='w-full mt-8'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Old Town San Diego
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={townRose} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Old Town San Diego
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={aguilarFamily} alt='' loading='eager' className='w-full mt-14'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 San Diego Pizza
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={laJolla2} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 La Jolla Cave
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={seaportFriends} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 SeaPort Village
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={littleItaly2} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Little Italy
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={imperial} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 Imperial Beach
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         {/* next start */}
-//         <div className='max-w-xs rounded overflow-hidden shadow-lg p-2'>
-//         {/* image next */}
-//         <img src={seaportMore} alt='' loading='eager' className='w-full'/>
-//         <div className='px-6 py-4'>
-//             <div className='font-bold text-purple-500 text-xl mb-2'>
-//                 SeaPort Village
-//             </div>
-//             <ul>
-//                 <li>
-//                     <strong>the view</strong>
-//                 </li>
-//             </ul>
-//         </div>
-//         </div>
-//         {/* next end */}
-
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Pictures;
-
-
-
+import sdSeaport from '../images/sd-seaport-village.jpeg'
 
 const Pictures = () => {
 
@@ -356,7 +47,7 @@ const Pictures = () => {
         {
             id: 4,
             src: aguilarFamily,
-            alt: "Image 3",
+            alt: "Image 4",
             name: "San Diego Pizza",
             description: "the view",
             date: "June 2022",
@@ -364,7 +55,7 @@ const Pictures = () => {
         {
             id: 5,
             src: seaport2,
-            alt: "Image 3",
+            alt: "Image 5",
             name: "SeaPort Village",
             description: "the view",
             date: "June 2022",
@@ -372,38 +63,106 @@ const Pictures = () => {
         {
             id: 6,
             src: centennialPark,
-            alt: "Image 3",
+            alt: "Image 6",
             name: "Centennial Park",
             description: "the view",
             date: "May 2024",
         },
         {
             id: 7,
-            src: cove,
-            alt: "Image 3",
-            name: "La Jolla Cove",
+            src: seaportMore,
+            alt: "Image 7",
+            name: "SeaPort Village",
             description: "the view",
+            date: "May 2024",
         },
         {
             id: 8,
-            src: cove,
-            alt: "Image 3",
-            name: "La Jolla Cove",
+            src: sdPier,
+            alt: "Image 8",
+            name: "San Diego Pier",
             description: "the view",
+            date: "May 2024",
         },
         {
             id: 9,
-            src: cove,
-            alt: "Image 3",
-            name: "La Jolla Cove",
+            src: sdSeaport,
+            alt: "Image 9",
+            name: "SeaPort Village",
             description: "the view",
+            date: "May 2024",
         },
         {
             id: 10,
+            src: seaportFriends,
+            alt: "Image 10",
+            name: "SeaPort Village",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 11,
+            src: Hero,
+            alt: "Image 11",
+            name: "Old Town San Diego",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 12,
             src: cove,
-            alt: "Image 3",
+            alt: "Image 12",
             name: "La Jolla Cove",
             description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 13,
+            src: laJolla,
+            alt: "Image 13",
+            name: "La Jolla Cove",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id:14,
+            src: laJolla2,
+            alt: "Image 14",
+            name: "La Jolla Cove",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 15,
+            src: cove,
+            alt: "Image 15",
+            name: "La Jolla Cove",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 16,
+            src: oldTown,
+            alt: "Image 16",
+            name: "Old Town San Diego",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 17,
+            src: townBeca,
+            alt: "Image 17",
+            name: "Old Town San Diego",
+            description: "the view",
+            date: "May 2024",
+        },
+        {
+            id: 18,
+            src: townRose,
+            alt: "Image 18",
+            name: "Old Town San Diego",
+            description: "the view",
+            date: "May 2024",
         },
     ];
 
@@ -412,7 +171,7 @@ const Pictures = () => {
             {/* San Diego Pictures Section */}
             <h3 className='text-2xl font-bold p-2'>San Diego</h3>
 
-            <div className='flex flex-wrap'>
+            <div className='flex flex-wrap justify-center'>
 
             {imagesList.map(( { id, src, alt, name, description, date }) => (
 
